ddfe43b526f693e7d23e3f5d86d31b4635a462a8
eyuel <eyuel.berga@yahoo.com>
2019-06-06 17:36:38 +0300

    postproductviewmodel added

---
+++ app/src/main/java/com/android/airmart/viewmodel/PostProductViewModel.kt
@@ -0,0 +1,22 @@
+package com.android.airmart.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.airmart.data.entity.Product
+import com.android.airmart.repository.ProductRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PostProductViewModel (private val productRepository: ProductRepository) : ViewModel(){
+    fun insertProduct (product: Product) = viewModelScope.launch (Dispatchers.IO){
+        productRepository.insertProduct(product)
+    }
+    fun updateProduct (product: Product) = viewModelScope.launch (Dispatchers.IO){
+        productRepository.updateProduct(product)
+    }
+    fun deleteProduct (product: Product) = viewModelScope.launch (Dispatchers.IO){
+        productRepository.deleteProduct(product)
+    }
+
+}
